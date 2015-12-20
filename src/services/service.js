fee5bb741d2dd22ccad63be426b5325dfffdc8f0
Jonathan Sweetman <jonaswee@ca.ibm.com>
2015-12-20 10:37:24 -0500

    can access on network

--- src/services/service.js
+++ src/services/service.js
@@ -1,12 +1,12 @@
 import http from './httpService';
-
+const host = '192.168.1.68';
 export default class Service {
 	static save(data) {
-		return http.post('http://localhost:3001/save', data );
+		return http.post('http://' + host + ':3001/save', data );
 	}
 
 	static characters() {
-		return http.get('http://localhost:3001/characters').then((data) => {
+		return http.get('http://' + host + ':3001/characters').then((data) => {
 			store.actions.rolePlayActions.characterRequestFinished(data);
 		}, (err) => {
 			console.error(err);
